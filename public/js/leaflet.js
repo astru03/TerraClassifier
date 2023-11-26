fab95fd9005367f9c3a9fb54b54d84829f178526
Andreas Struffert-Froböse <a_stru03@uni-muenster.de>
2023-11-26 12:14:49 +0100

    modified popup-fenster

--- public/js/leaflet.js
+++ public/js/leaflet.js
@@ -19,17 +19,37 @@ map.addControl(new L.Control.Draw( {
     draw: {
         polyline: false,
         rectangle: true,
-        polygon: true,
+        polygon: false,
         circle: false,
         circlemarker: false,
         marker: false
     }
 })) 
 
+var rectangleCoordinates = null;
 // Event-Handler for drawing polygons
 map.on("draw:created", function(event){
-    var layer = event.layer;
+  var type = event.layerType,
+  layer = event.layer;
+
+  if (type == 'rectangle') {
+    //rectangleCoordinates = layer.getBounds().toBBoxString();
+    rectangleCoordinates = layer.getBounds();
+    console.log(rectangleCoordinates); 
+  }
     drawnFeatures.addLayer(layer);
+})
+
+// Event-Handler for editing rectangle
+map.on("draw:edited", function(event){
+  var layers = event.layers;
+  layers.eachLayer(function (layer) {
+    if (layer instanceof L.Rectangle) {
+      //rectangleCoordinates = layer.getBounds().toBBoxString();
+      rectangleCoordinates = layer.getBounds();
+      //console.log('Edited Rectangle Coordinates:', rectangleCoordinates);
+    }
+  });
 })
 
 // show the scale bar on the lower left corner
@@ -41,49 +61,86 @@ L.control.scale({imperial: true, metric: true}).addTo(map);
 //1. Funktion darf nur ausgeführt werden, wenn auch ein AOI über das Rechteck ausgewählt wurde.
 //2. Funktion darf nicht ausgeführt werden, wenn ein AOI über ein Polygon ausgewählt wurde. (Wenn möglich)
 //3. Funktion darf nicht ausgeführt werden, wenn kein AOI gewählt wurde
-function showAlert1() {
-    alert('Option 1 wurde geklickt!');
-  }
-
-  function showAlert2() {
-    alert('Option 2 wurde geklickt!');
-  }
-
-  function showAlert3() {
-    alert('Option 3 wurde geklickt!');
-  }
-
-  // Erstelle EasyButtons für die Aktionen des Menüs
-  var button1 = L.easyButton('<img src="https://raw.githubusercontent.com/astru03/TerraClassifier/main/public/images/sentinal_icon.png" style="width: 20px; height: 20px;">', showAlert1, 'Sentinal-2');
-  var button2 = L.easyButton('<img src="https://raw.githubusercontent.com/astru03/TerraClassifier/main/public/images/trainigsdaten_icon.png" style="width: 20px; height: 20px;">', showAlert2, 'Trainigsdaten');
-  var button3 = L.easyButton('<img src="https://raw.githubusercontent.com/astru03/TerraClassifier/main/public/images/algorithmus_icon.png" style="width: 20px; height: 20px;">', showAlert3, 'Algorithmus');
+function showAlert1(coordinates) {
+  let NorthEastCoordinates = 'Lat: ' + coordinates.getNorthEast().lat.toFixed(4) + ' ; Lng: ' + coordinates.getNorthEast().lng.toFixed(4);
+  console.log(NorthEastCoordinates);
+  let SouthwestCoordinates = 'Lat: ' + coordinates.getSouthWest().lat.toFixed(4) + ' ; Lng: ' + coordinates.getSouthWest().lng.toFixed(4);
+  console.log(SouthwestCoordinates);
+  var popup = document.getElementById('popup');
+  popup.style.display = 'block';
+  var recNortheastCoords = document.getElementById('northeastCoordinates');
+  var recSouthwestCoords = document.getElementById('southwestCoordinates');
+  recNortheastCoords.innerHTML = NorthEastCoordinates;
+  recSouthwestCoords.innerHTML = SouthwestCoordinates;
   
-  // Erstelle den Haupt-Button (Burgermenü-Button)
-  var toggleMenuButton = L.easyButton({
-    position: 'topright',
-    states: [{
-      stateName: 'closed',
-      icon: '<img src="https://raw.githubusercontent.com/astru03/TerraClassifier/main/public/images/menu_icon.png" style="width: 20px; height: 20px;">',
-      title: 'Öffne Menü',
-      onClick: function (btn, map) {
-        btn.state('open');
-        button1.addTo(map).setPosition('topright');
-        button2.addTo(map).setPosition('topright');
-        button3.addTo(map).setPosition('topright');
-      }
-    }, {
-      stateName: 'open',
-      icon: '<img src="https://raw.githubusercontent.com/astru03/TerraClassifier/main/public/images/menu_icon.png" style="width: 20px; height: 20px;">',
-      title: 'Schließe Menü',
-      onClick: function (btn, map) {
-        btn.state('closed');
-        button1.remove();
-        button2.remove();
-        button3.remove();
-      }
-    }]
+  //Datepicker
+  $('#popup').show();
+  initDatepickers();
+}
+
+function initDatepickers() {
+  $('#fromDate').datepicker({
+    dateFormat: 'dd.mm.yy' // Format des Datums
   });
 
-  // Füge den Haupt-Button zur Karte hinzu
-  toggleMenuButton.addTo(map);
+  $('#toDate').datepicker({
+    dateFormat: 'dd.mm.yy' // Format des Datums
+  });
+}
+
+
+function showAlert2() {
+    alert('Option 2 wurde geklickt!');
+}
+
+function showAlert3() {
+    alert('Option 3 wurde geklickt!');
+}
+
+function closePopup() {
+    var popup = document.getElementById('popup');
+    popup.style.display = 'none';
+}
+
+// Erstelle EasyButtons für die Aktionen des Menüs
+var button1 = L.easyButton('<img src="https://raw.githubusercontent.com/astru03/TerraClassifier/main/public/images/sentinal_icon.png" style="width: 20px; height: 20px;">', function() {
+  if(rectangleCoordinates) {
+    //console.log(rectangleCoordinates);
+    showAlert1(rectangleCoordinates)
+  } else {
+    console.log("Es wurde kein Rechteck gezeichnet!");
+  }
+}, 'Sentinal-2');
+  
+var button2 = L.easyButton('<img src="https://raw.githubusercontent.com/astru03/TerraClassifier/main/public/images/trainigsdaten_icon.png" style="width: 20px; height: 20px;">', showAlert2, 'Trainigsdaten');
+var button3 = L.easyButton('<img src="https://raw.githubusercontent.com/astru03/TerraClassifier/main/public/images/algorithmus_icon.png" style="width: 20px; height: 20px;">', showAlert3, 'Algorithmus');
+  
+// Erstelle den Haupt-Button (Burgermenü-Button)
+var toggleMenuButton = L.easyButton({
+  position: 'topright',
+  states: [{
+    stateName: 'closed',
+    icon: '<img src="https://raw.githubusercontent.com/astru03/TerraClassifier/main/public/images/menu_icon.png" style="width: 20px; height: 20px;">',
+    title: 'Öffne Menü',
+    onClick: function (btn, map) {
+      btn.state('open');
+      button1.addTo(map).setPosition('topright');
+      button2.addTo(map).setPosition('topright');
+      button3.addTo(map).setPosition('topright');
+    }
+  }, {
+    stateName: 'open',
+    icon: '<img src="https://raw.githubusercontent.com/astru03/TerraClassifier/main/public/images/menu_icon.png" style="width: 20px; height: 20px;">',
+    title: 'Schließe Menü',
+    onClick: function (btn, map) {
+      btn.state('closed');
+      button1.remove();
+      button2.remove();
+      button3.remove();
+    }
+  }]
+});
+
+// Füge den Haupt-Button zur Karte hinzu
+toggleMenuButton.addTo(map);
 
